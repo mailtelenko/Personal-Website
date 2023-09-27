6f1b9d63231f698c8d191fe3860fcf73401cd02a
Liam Telenko <telenkol@mcmaster.ca>
2023-09-26 23:20:45 -0400

    Clean-up of ResumeElement. Timeline fix

--- src/components/ResumeElement.vue
+++ src/components/ResumeElement.vue
@@ -1,38 +1,41 @@
 <template>
-  <div v-bind:class = "{'expand' : expand_element}">
+  <div>
     <div class="template_pin"> </div>
     <div
       v-on:click      = "toggle_element"
       v-click-outside = "hide"
-      :class          = "{'expanded' : expand_element}"
+      :class          = "{'expand' : isActive}"
       v-bind:style    = "{'background-image': 'linear-gradient(140deg, var(--panel_colour), 78%, ' + element_data.accent + '), url(/images/' + element_data.image + ')'}"
-      class           = "resume_element_container"   
+      id              = "resume_element_container"   
     >
-      <div class="resume_header">
-        <h3>{{ element_data.name }}</h3>
-        <p class="year">{{ element_data.dates }}</p>
+      <div class = "resume_header">
+        <h3 id = "name">{{ element_data.name }}</h3>
+        <p  id = "year">{{ element_data.dates }}</p>
 
-        <div class="intro">
+        <div id = "intro">
           <p v-html="element_data.description"></p>
         </div>
       </div>
 
       <div 
-        :class = "{'expand_below_fold': expand_element}" 
-        class  = "below_fold"
+        :class = "{'expand': isActive}" 
+        id     = "content"
       >
         <p 
           v-show = "element_data.location != null" 
-          class  = "location"
+          id     = "location"
         >
-          <font-awesome-icon class="icon" icon="map-marker-alt" />
+          <font-awesome-icon 
+            id   = "icon"
+            icon = "map-marker-alt" 
+          />
           {{ element_data.location }}
         </p>
 
         <div
           id           = "content_container"
           v-on:click   = "click_point"
-          v-bind:class = "{'expand' : expand_element}"
+          v-bind:class = "{'expand' : isActive}"
         >
           <CardScroll :element_data="element_data"></CardScroll>
         </div>
@@ -65,7 +68,7 @@
 
     TODO: Fix hover
   */
-  top:  calc(130px/2 - 8px - 5px);
+  top:  calc(140px/2 - 8px - 5px);
 
 
   border:        solid 5px var(--panel_dark_embed);
@@ -86,7 +89,7 @@
 /*
   Parent div for the resume element box
 */
-.resume_element_container {  
+#resume_element_container {  
 
   /* 
     We have to specify the max height here instead of just height
@@ -97,10 +100,6 @@
 
   position: relative;
   display:  block;
-
-  /* Configure the interior as a grid */
-  /* grid-template-columns: 45% 55%; */
-  /* grid-template-rows:    auto; */
 
   /* Configure the background */
   background-size:     cover;
@@ -115,13 +114,29 @@
   transition-duration: 0.8s;
 }
 
-.expand > div {
+#resume_element_container.expand {
   max-height: 1000px;
 
   overflow-x: visible; /* TODO: Needed? */
 
   transition-duration: 1s;
 }
+
+/*
+  Hover animation for the entire element
+*/
+#resume_element_container:hover {
+  box-shadow: -3px 8px 20px var(--box_shadow_colour_intense);
+}
+
+/*
+  When the element is not active, increase
+  the height slightly when hovered
+*/
+#resume_element_container:hover:not(.expand) {
+  max-height: 150px;
+}
+
 
 /*
   Header of the element. Visible before and after the element is
@@ -143,110 +158,83 @@
   padding-top:  15px;
   padding-left: 20px;
 
-  background-color: var(--panel_header);
-
   transition-duration: .7s;
 }
 
+/*
+  Remove the default padding from the text elements
+*/
+.resume_header #name, #year, #intro {
+  margin: 0px;
+}
+
+/*
+  Adjust the header when the element is expanded
+*/
 .expand .resume_header {
   height: 160px;
 
-  padding-top: 25px;
+  padding-top:  25px;
   padding-left: 35px;
 
   border-bottom-left-radius:  10px;
   border-bottom-right-radius: 10px;
 
+  background-color: var(--panel_header);
 }
 
 
 /* 
-  Title styling
+  Header text styling
+  Only visible after expansion
 */
-.resume_element h3 {
-  height: auto;
-  margin: 0px;
-
-  white-space: nowrap;
-
+#name {
   font-size: 1.5rem;
 }
 
-.resume_element.half h3, .resume_element.slim h3 {
-  white-space: normal;
-}
-
-.resume_element .year {
-  margin:     0px;
-  margin-top: 11px;
-
-  height: auto;
-
+#year {
   font-size: 1.1rem;
+
+  margin-top: 11px;
 }
 
-.resume_element .intro {
-  margin-top: 0px;
-
-  width: 100%;
-
+#intro {
   font-size: 1.15rem;
-  word-wrap: none;
 }
 
-.resume_element .location {
+
+/*
+  Header location pin
+*/
+#location {
   position: absolute;
 
   right: 45px;
-  top: 20px;
+  top:   20px;
 
   text-shadow: 0px 0px 3px var(--box_shadow_colour_intense);
 }
 
-.icon {
+#icon {
   padding-right: 10px;
 }
 
-.resume_element .intro p {
-  display: inline-block;
-  vertical-align: middle;
-  line-height: normal;
-}
 
-.resume_element > div:hover {
-  max-height: 150px;
-
-  box-shadow: -3px 8px 20px var(--box_shadow_colour_intense);
-}
-
-.expanded:hover {
-  max-height: 800px !important;
-
-  box-shadow: -3px 8px 20px var(--box_shadow_colour_intense);
-}
-
-.below_fold {
-  width: 100%;
-
-  display: inline-block;
-
+/*
+  Below the fold content
+*/
+#content {
   max-height: 0px;
-
-  overflow: visible;
 
   opacity: 0;
 
   transition-duration: 0.8s;
 }
 
-.expand_below_fold {
+#content.expand {
   max-height: 1000px;
 
-  overflow: visible;
-
   opacity: 1;
-
-  transition-duration: 0.8s;
 }
 </style>
 
@@ -266,23 +254,23 @@ export default {
 
   data: function() {
     return {
-      expand_element: true
+      isActive: true
     };
   },
 
   methods: {
     hide() {
-      this.expand_element = false;
+      this.isActive = false;
     },
 
     toggle_element() {
       if (this.display_props.expand) {
-        this.expand_element = !this.expand_element;
+        this.isActive = !this.isActive;
       }
     },
 
     click_point(event) {
-      if (this.expand_element) 
+      if (this.isActive) 
         event.stopPropagation();
     }
   }
--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -16,12 +16,17 @@
   width:            4px;
 
   /* TODO: If expanded, need to reduce the amount of height again */
-  height:           calc(100% - 140px - 30px);
+  /*
+    Size of element is 140px
+    30px of margin between elements
+    Timeline button height is 16px
+  */
+  height:           calc(100% - 140px - 30px - 16px);
 
   overflow:         visible;
 
   /* Height of minimized element is 130px */
-  top:  calc(130px / 2);
+  top:  calc(140px / 2);
 
   /* Total width of the timeline block is 90px
      Width of the timeline itself is 4px. */
